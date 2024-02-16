54068b81803176811aa18ace35c900c6aa9b7a8d
Dimuthu Abeysinghe <41988837+Dimuthu7@users.noreply.github.com>
2024-02-16 19:09:51 +0530

    Add files via upload

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,14 +1,22 @@
 'use client'
 
 import { useParamsStore } from '@/hooks/useParamsStore'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 import { FaCarAlt } from 'react-icons/fa'
 
 export const Logo = () => {
+    const router = useRouter();
+    const pathname = usePathname();
     const reset = useParamsStore(state => state.reset);
 
+    const doReset = () => {
+        if(pathname !== '/') router.push('/');
+        reset();
+    }
+
     return (
-        <div onClick={reset} 
+        <div onClick={doReset} 
             className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
             <FaCarAlt size={34}/>
             <div>Carsties Auctions</div>
--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import { FaCarAlt } from "react-icons/fa";
 import { Search } from './Search';
 import { Logo } from './Logo';
+import { LoginButton } from './LoginButton';
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export const Navbar = () => {
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className='
         sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
     '>
         <Logo />
         <Search />
-        <div>Login</div>
+        {user ? (
+          <UserActions user={user}/>
+        ) : (
+          <LoginButton />
+        )}
     </header>
   )
 }
