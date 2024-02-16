6c0fc2795761509716b8fa6f7aa7b7e13ca4c7b1
Dimuthu Abeysinghe <41988837+Dimuthu7@users.noreply.github.com>
2024-02-16 19:08:27 +0530

    Add new actions

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -1,6 +1,7 @@
 'use server'
 
 import { Auctions, PageResult } from "@/types";
+import { getTokenWorkaround } from "./authActions";
 
 export async function getData(query: string): Promise<PageResult<Auctions>>{
     console.log(`http://localhost:6001/search${query}`);
@@ -10,3 +11,24 @@ export async function getData(query: string): Promise<PageResult<Auctions>>{
 
     return res.json();
 } 
+
+export async function UpdateAuctionTest() {
+    const data = {
+        milage: Math.floor(Math.random() * 100000) + 1
+    }
+
+    const token = await getTokenWorkaround();
+
+    const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': 'Bearer ' + token?.access_token
+        },
+        body: JSON.stringify(data)
+    })
+
+    if(!res.ok) return { status: res.status, message: res.statusText }
+
+    return res.statusText;
+}
